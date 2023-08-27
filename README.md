da43f9b7252e1e5569c8541c7292f6a7546f9585
tcskiran <63388243+tcskiran@users.noreply.github.com>
2023-08-27 23:45:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Steps to run the extension
+
+1. Clone this repository
+
+2. In Chrome, open [chrome://extensions/](chrome://extensions/)
+
+3. Click Developer mode
+
+4. Click Load unpacked extension…
+
+5. Navigate to the cloned folder and click OK
+
+6. Pin the extension and it is ready to use
---
+++ popup.js
@@ -0,0 +1,132 @@
+// DOM Element References
+const siteList = document.getElementById('siteList');
+const noteArea = document.getElementById('noteArea');
+
+// Load all saved site URLs into the dropdown on popup load
+chrome.storage.local.get(null, (items) => {
+  for (const url in items) {
+    const option = document.createElement('option');
+    option.value = url;
+    option.textContent = url;
+    siteList.appendChild(option);
+  }
+});
+
+// Display note of the selected site
+siteList.addEventListener('change', () => {
+  const url = siteList.value;
+  chrome.storage.local.get(url, (result) => {
+    noteArea.value = result[url] || '';
+  });
+});
+
+document.getElementById('saveNote').addEventListener('click', () => {
+  let url = siteList.value;
+  if (!url || url === undefined) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      url = tabs[0].url;
+      saveNote(url);
+    });
+  } else {
+    saveNote(url);
+  }
+});
+
+function saveNote(url) {
+  const note = noteArea.value;
+  const noteObject = {};
+  noteObject[url] = note;
+
+  chrome.storage.local.set(noteObject, () => {
+    alert('Note saved!');
+  });
+}
+
+// Set the dropdown value to the current site when popup is opened
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const url = tabs[0].url;
+
+  // Show the URL if it is already saved
+  for (let i = 0; i < siteList.options.length; i++) {
+    if (siteList.options[i].value === url) {
+      siteList.value = url;
+      chrome.storage.local.get(url, (result) => {
+        noteArea.value = result[url] || '';
+      });
+      break;
+    }
+  }
+});
+
+// Add an event listener for the delete button
+document.getElementById('deleteNote').addEventListener('click', () => {
+  let url = siteList.value;
+  if (!url) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      url = tabs[0].url;
+      deleteNote(url);
+    });
+  } else {
+    deleteNote(url);
+  }
+});
+
+function deleteNote(url) {
+  chrome.storage.local.remove(url, () => {
+    alert('Note deleted!');
+
+    // Remove the URL from the dropdown list
+    for (let i = 0; i < siteList.options.length; i++) {
+      if (siteList.options[i].value === url) {
+        siteList.remove(i);
+        break;
+      }
+    }
+
+    // Clear the textarea
+    noteArea.value = '';
+  });
+}
+
+document.getElementById('exportNotes').addEventListener('click', () => {
+  chrome.storage.local.get(null, (data) => {
+    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    // Create a link element to start download
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'notes_backup.json';
+    document.body.appendChild(a);
+    a.click();
+
+    // Cleanup
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  });
+});
+
+document.getElementById('importNotes').addEventListener('click', () => {
+  const fileInput = document.getElementById('importFile');
+  fileInput.click();
+
+  fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file, 'UTF-8');
+
+      reader.onload = (event) => {
+        try {
+          const data = JSON.parse(event.target.result);
+          chrome.storage.local.set(data, () => {
+            alert('Notes imported successfully!');
+          });
+        } catch (error) {
+          alert('Failed to import notes. Invalid file format.');
+        }
+      };
+    }
+  });
+});
